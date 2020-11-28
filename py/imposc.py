2e3dc6b5d1a2e78f241b5a047e5e872703df8cfe
Felix Dux <felixjdux@gmail.com>
2020-11-28 19:10:02 +0000

    abstracting symbol names in python binding

--- py/imposc.py
+++ py/imposc.py
@@ -1,5 +1,6 @@
 import ctypes
 import pathlib
+from subprocess import Popen, PIPE
 
 
 def wrap_string(s: str):
@@ -11,28 +12,39 @@ class ImposcIF:
 
     def __init__(self, images: pathlib.Path):
         # Load the shared library into ctypes
-        libname = pathlib.Path(__file__).parent.parent.absolute() / "build/imposcpy/libimposcpy.so"
-        self.c_lib = ctypes.CDLL(libname)
+        self.libname = pathlib.Path(__file__).parent.parent.absolute() / "build/imposcpy/libimposcpy.so"
+        self.c_lib = ctypes.CDLL(self.libname)
 
-        self.c_lib._Z11map_impactsdddjddjPKc.restype = ctypes.c_bool
-        self.c_lib._Z19map_singularity_setdddjjPKc.restype = ctypes.c_bool
-        self.c_lib._Z7map_doadddjdjjjPKc.restype = ctypes.c_bool
+        self.functions = dict()
+
+        self.add_function("map_impacts")
+        self.add_function("map_singularity_set")
+        self.add_function("map_doa")
 
         self.images = images.absolute()
+
+    def symbol(self, name: str) -> str:
+        proc_nm = Popen(["nm", "-D", self.libname], stdout = PIPE)
+        proc_grep = Popen(["grep", name], stdin=proc_nm.stdout, stdout=PIPE)
+        return proc_grep.communicate()[0].split()[2].decode()
+
+    def add_function(self, name: str):
+        self.functions[name] = getattr(self.c_lib, self.symbol(name))
+        self.functions[name].restype = ctypes.c_bool
 
     def image_file(self, outfile: str):
         return wrap_string(str(self.images / outfile))
 
     def impacts(self, omega: float, r: float, sigma: float, max_periods: int, phi: float, v: float, num_iterations: int, outfile: str) -> bool:
-        return self.c_lib._Z11map_impactsdddjddjPKc(ctypes.c_double(omega), ctypes.c_double(r), ctypes.c_double(sigma), ctypes.c_uint(max_periods),
+        return self.functions["map_impacts"](ctypes.c_double(omega), ctypes.c_double(r), ctypes.c_double(sigma), ctypes.c_uint(max_periods),
                 ctypes.c_double(phi), ctypes.c_double(v), ctypes.c_uint(num_iterations), self.image_file(outfile))
 
     def singularity_set(self, omega: float, r: float, sigma: float, max_periods: int, num_points: int, outfile: str) -> bool:
-        return self.c_lib._Z19map_singularity_setdddjjPKc(ctypes.c_double(omega), ctypes.c_double(r), ctypes.c_double(sigma), ctypes.c_uint(max_periods),
+        return self.functions["map_singularity_set"](ctypes.c_double(omega), ctypes.c_double(r), ctypes.c_double(sigma), ctypes.c_uint(max_periods),
                 ctypes.c_uint(num_points), self.image_file(outfile))
 
     def doa(self, omega: float, r: float, sigma: float, max_periods: int, max_velocity: float, n_v_increments: int, n_phi_increments: int, outfile: str) -> bool:
-        return self.c_lib._Z7map_doadddjdjjjPKc(ctypes.c_double(omega), ctypes.c_double(sigma), ctypes.c_uint(max_periods),
+        return self.functions["map_doa"](ctypes.c_double(omega), ctypes.c_double(sigma), ctypes.c_uint(max_periods),
                 ctypes.c_double(max_velocity), ctypes.c_uint(n_v_increments), ctypes.c_uint(n_phi_increments), self.image_file(outfile))
 
 
@@ -40,4 +52,4 @@ if __name__ == "__main__":
 
     imposc = ImposcIF(pathlib.Path(__file__).parent)
 
-    imposc.impacts(5.2, 0.8, 0, 100, 0.5, 0.5, 1000, "myfile.png")
+    imposc.impacts(2.8, 0.8, 0, 100, 0.5, 0.5, 1000, "myfile.png")
