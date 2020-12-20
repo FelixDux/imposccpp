3185365e84629aa3f2cc32b6d47db822615ce347
Felix Dux <felixjdux@gmail.com>
2020-12-20 21:39:20 +0000

    Some TODOs

--- imposc-ui/src/index.js
+++ imposc-ui/src/index.js
@@ -15,3 +15,16 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// TODO: comms to imposc service
+// TODO: display imposc response
+// TODO: form element to send imposc request
+// TODO: arrange SPA with form and display elements
+// TODO: make imposc service address and port number configurable
+// TODO: Make imposc service self-documenting via JSON (actions, descriptions, variable lists, with descriptions - try to follow same structure as Elixir project)
+// TODO: Construct form element from actions JSON
+// TODO: don't forget unit tests, docstrings, comments
+// TODO: Dockerise
+// TODO: Orchestrate with K8s?
+// TODO: Pull Elixir project into same structure
+// TODO: create alternate imposc services in Rust, Golang, Haskell, Java
