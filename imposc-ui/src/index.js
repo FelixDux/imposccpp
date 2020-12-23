1484a82cc5362d162d943940eeb328b1d2847597
Felix Dux <felixjdux@gmail.com>
2020-12-23 12:28:15 +0000

    Configurable input fields

--- imposc-ui/src/index.js
+++ imposc-ui/src/index.js
@@ -4,6 +4,57 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class PlotterInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const step = this.props.type === 'integer' ? 1 : 0.01;
+    let min = '';
+    let max = '';
+
+    switch (this.props.range)
+    {
+      case 'non-negative': {
+        min = 0;
+        break;
+      }
+      case 'negative': {
+        max = -step;
+        break;
+      }
+      case 'non-positive': {
+        max = 0;
+        break;
+      }
+      case 'positive': {
+        min = step;
+        break;
+      }
+      case 'circle' : {
+        min = 0;
+        max = 1;
+        break;
+      }
+    }
+
+    return (
+      <label>
+        {this.props.label}: <input 
+          name={this.props.name}
+          type="number"
+          value={this.props.value}
+          onChange={this.props.onChange}
+          step = {step}
+          min = {min}
+          max = {max}
+        />
+      </label>
+    )
+  }
+}
+
 class PlotterForm extends React.Component {
   constructor(props) {
     super(props);
@@ -38,7 +89,7 @@ class PlotterForm extends React.Component {
       })
     .then(response => {
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error('Network response failed');
       }
       return response.blob();
     })
@@ -46,121 +97,49 @@ class PlotterForm extends React.Component {
       this.setState( {blob: blob, src: URL.createObjectURL(blob), result: "Done"});
     })
     .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error);
+      console.error('Fetch operation failed:', error);
       this.setState({result: error.toString()});
     });
     event.preventDefault();
+  }
+
+  renderInput(name, label, value, range, type)
+  {
+    return (
+      <PlotterInput 
+        name = {name}
+        label = {label}
+        value = {value}
+        range = {range}
+        type = {type}
+        onChange={this.handleInputChange}
+      />
+    )
   }
 
   render() {
     return (
       <div>
       <form onSubmit={this.handleSubmit}>
-      <label>Omega: <input 
-        name="omega"
-        type="number"
-        value={this.state.args.omega}
-        onChange={this.handleInputChange} />
-      </label>
-      <label>Sigma: <input 
-        name="sigma"
-        type="number"
-        value={this.state.args.sigma}
-        onChange={this.handleInputChange}
-        />
-      </label>
-      <label>r: <input 
-        name="r"
-        type="number"
-        value={this.state.args.r}
-        onChange={this.handleInputChange}
-        />
-      </label>
-      <label>phi: <input 
-        name="phi"
-        type="number"
-        value={this.state.args.phi}
-        onChange={this.handleInputChange}
-        />
-      </label>
-      <label>v: <input 
-        name="v"
-        type="number"
-        value={this.state.args.v}
-        onChange={this.handleInputChange}
-        />
-      </label>
-      <label>max_periods: <input 
-        name="max_periods"
-        type="number"
-        value={this.state.args.max_periods}
-        onChange={this.handleInputChange}
-        />
-      </label>
-      <label>num_impacts: <input 
-        name="num_impacts"
-        type="number"
-        value={this.state.args.num_impacts}
-        onChange={this.handleInputChange}
-        />
-      </label>
+      {this.renderInput('omega', 'Forcing frequency', this.state.args.omega, 'positive')}
+      {this.renderInput('sigma', 'Obstacle offset', this.state.args.sigma)}
+      {this.renderInput('r', 'Coefficient of restitution', this.state.args.r, 'non-negative')}
+      {this.renderInput('phi', 'Initial phase', this.state.args.phi, 'circle')}
+      {this.renderInput('v', 'Initial velocity', this.state.args.v, 'non-negative')}
+      {this.renderInput('max_periods', 'Max forcing periods between impacts', this.state.args.max_periods, 'positive', 'integer')}
+      {this.renderInput('num_impacts', 'Number of impacts to plot', this.state.args.num_impacts, 'positive', 'integer')}
       <input type="submit" value="Update" />
       </form>
-      <div><img src={this.state.src} alt="" /></div>
+      <div align="center" ><img src={this.state.src} alt="" width="50%" align="center" /></div>
       </div>
     )
   }
 
 };
 
-class Plotter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      blob: null,
-      src: "",
-      result: "Waiting"
-    };
-  }
-
-  handleButtonClick = () => {
-    fetch(this.props.url,
-      {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-                    'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(this.props.args)
-      })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.blob();
-    })
-    .then(blob => {
-      this.setState( {blob: blob, src: URL.createObjectURL(blob), result: "Done"});
-    })
-    .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error);
-      this.setState({result: error.toString()});
-    });
-  };
-
-  render() {
-    return (
-      <div><div><h1>{this.state.result}</h1><button onClick={this.handleButtonClick}>Update</button></div><div><img src={this.state.src} alt="" /></div></div>
-    );
-  }
-};
-
 ReactDOM.render(
   <PlotterForm 
   url = 'http://127.0.0.1:5000/impacts'
-  // args = {{
-  //   omega: 5.2, sigma: 0.0, r: 0.8, phi: 0.2, v: 0.01, max_periods: 100, num_impacts: 5000,
-  // }}
   />,
   document.getElementById('root')
 );
@@ -170,10 +149,10 @@ ReactDOM.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-// TODO: arrange SPA with form and display elements
-// TODO: make imposc service address and port number configurable
 // TODO: Make imposc service self-documenting via JSON (actions, descriptions, variable lists, with descriptions - try to follow same structure as Elixir project)
 // TODO: Construct form element from actions JSON
+// TODO: arrange SPA with form and display elements
+// TODO: make imposc service address and port number configurable
 // TODO: don't forget unit tests, docstrings, comments
 // TODO: Dockerise
 // TODO: Orchestrate with K8s?
