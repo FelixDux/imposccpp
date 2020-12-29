188ab1e22020c96f4fbc1f3c16244cb9be380f6a
Felix Dux <felixjdux@gmail.com>
2020-12-29 13:02:02 +0000

    refactored app structure

--- imposc-ui/src/App.js
+++ imposc-ui/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import PlotterApp from './plotter'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Impact Oscillator</h1>
       </header>
+      <body>
+      <PlotterApp 
+        url = 'http://127.0.0.1:5000'
+      />
+      </body>
     </div>
   );
 }
 
 export default App;
+
+// TODO: make imposc service address and port number configurable
+// TODO: proper passing-through of error messages
+// TODO: don't forget unit tests, docstrings, comments
+// TODO: tidy up plotting legends etc
+// TODO: move to using react-router for navigation
+// TODO: add page on mathematical background
+// TODO: Dockerise
+// TODO: Orchestrate with K8s?
+// TODO: Pull Elixir project into same structure
+// TODO: create alternate imposc services in Rust, Golang, Haskell, Java
