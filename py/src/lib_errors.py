ba45567dd208657a6523784fcfcd8f4c84a19082
Felix Dux <felixjdux@gmail.com>
2020-12-30 11:45:34 +0000

    unit testing lib errors

--- py/src/lib_errors.py
+++ py/src/lib_errors.py
@@ -3,9 +3,16 @@ from pathlib import Path
 from file_makers import unique_file_name, files_size
 
 class LibErrors:
+    """ Maintains a per-session log of error messages """
 
-    def __init__(self, _directory: str = "errors"):
-        errors_path = Path(_directory).absolute()
+    def __init__(self, directory: str = "errors"):
+        """
+
+        Keyword arguments:
+
+        directory -- the name of a directory to store error logs in (default 'errors')
+        """
+        errors_path = Path(directory).absolute()
 
         if not errors_path.exists():
             errors_path.mkdir()
@@ -13,19 +20,21 @@ class LibErrors:
         self._log = errors_path / unique_file_name("txt")
 
     @property
-    def errorFile(self):
+    def errorFile(self) -> str:
         return str(self._log)
 
     @property
-    def errorPath(self):
+    def errorPath(self) -> Path:
         return self._log
 
-    def read(self):
+    def read(self) -> str:
+        """ Returns the contents of the error log """
         if self._log.exists():
             return self._log.read_text()
         else:
             return ""
 
     def put(self, message):
+        """ Logs an error message """
         with self._log.open(mode='a') as stream:
-            stream.writelines([message])
+            stream.write(f"{message}\n")
