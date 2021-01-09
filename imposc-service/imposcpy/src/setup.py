9263cba874ecf8bab65a2eec043074673047227b
Felix Dux <felixjdux@gmail.com>
2021-01-09 09:48:58 +0000

    Flask service in Docker container was unreachable - fixed by introducing gunicorn

--- imposc-service/imposcpy/src/setup.py
+++ imposc-service/imposcpy/src/setup.py
@@ -15,14 +15,12 @@ package_module = this_dir / "imposc_library"
 package_pyx_file = package_module.with_suffix(".pyx")
 package_cpp_file = package_module.with_suffix(".cpp")
 
-# TODO: now encountering this issue in Docker https://stackoverflow.com/questions/492374/g-in-what-order-should-static-and-dynamic-libraries-be-linked/492498#492498
-
+# The libraries argument is needed to avoid an issue with link order -
+# see https://stackoverflow.com/questions/492374/g-in-what-order-should-static-and-dynamic-libraries-be-linked/492498#492498
 imposc_extension = Extension(
     name="imposc_library",
     sources=["imposc_library.pyx"],
     libraries=["stdc++fs"],
-    # library_dirs=[libs_path_str],
-    # include_dirs=[libs_path_str],
     extra_compile_args = ["-std=c++17"]
 )
 
